fb0435a9997756861e4ac9b80a63a37ef378b335
Rozbita_Zaluzie <restik.nosek@gmail.com>
2022-05-18 13:26:36 +0200

    improved animation

--- toaster.js
+++ toaster.js
@@ -1,14 +1,24 @@
-function openToast() {
+function openToast(clicked_id) {
     var element = document.getElementById("toaster");
-
+    var content = document.getElementById("toasterContent");
+    
     // based on button dc or email
     try {
-        navigator.clipboard.writeText("Rozbita_Zaluzie#2703");
+        if (clicked_id == "dc") {
+            navigator.clipboard.writeText("Rozbita_Zaluzie#2703");
+            content.innerHTML = "Rozbita_Zaluzie#2703"
+        } else {
+            navigator.clipboard.writeText("restik.nosek@gmail.com");
+            content.innerHTML = "restik.nosek@gmail.com"
+        }
     } catch (error) {}
-
+   
+    // start animation
     element.style.display = "block"
     element.classList.add('copiedToast');
 
+
+    // end animation
     window.setTimeout(function() {
         element.style.display = "none"
         element.classList.remove('copiedToast');
